807200e2b80971b573606897303e614776cd92f1
sunzhenliang <hisunzhenliang@outlook.com>
2022-01-12 00:15:38 +0800

    step 5

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/cupertino.dart';
-import 'styles.dart';
+import 'package:flutter/services.dart'; //11
+import 'package:cupertino_store/product_list_tab.dart';
+import 'package:cupertino_store/search_tab.dart';
+import 'package:cupertino_store/shopping_cart_tab.dart';
+import 'package:cupertino_store/styles.dart';
 
 class CupertinoStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // This app is designed only to work vertically, so we limit
+    // orientations to portrait up and down.
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
     return CupertinoApp(
       home: CupertinoStoreHomePage(),
     );
@@ -13,11 +22,68 @@ class CupertinoStoreApp extends StatelessWidget {
 class CupertinoStoreHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: const Text('Cupertino Store'),
+    // return CupertinoPageScaffold(
+    //   navigationBar: CupertinoNavigationBar(
+    //     middle: const Text('Cupertino Store'),
+    //   ),
+    //   child: Container(),
+    // );
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
       ),
-      child: Container(),
+      tabBuilder: (context, index) {
+        CupertinoTabView returnValue = CupertinoTabView(
+          builder: (context) {
+            return CupertinoPageScaffold(
+              child: ProductListTab(),
+            );
+          },
+        );
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return CupertinoPageScaffold(
+                  child: ProductListTab(),
+                );
+              },
+            );
+            break;
+          case 1:
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return CupertinoPageScaffold(
+                  child: SearchTab(),
+                );
+              },
+            );
+            break;
+          case 2:
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return CupertinoPageScaffold(
+                  child: ShoppingCartTab(),
+                );
+              },
+            );
+            break;
+        }
+        return returnValue;
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
+import 'model/app_state_model.dart';
 import 'app.dart';
 
 void main() {
-  // This app is designed only to work vertically, so we limit
-  // orientations to portrait up and down.
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-  return runApp(CupertinoStoreApp());
+  // return runApp(CupertinoStoreApp());
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (context) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreApp(),
+    ),
+  );
 }
